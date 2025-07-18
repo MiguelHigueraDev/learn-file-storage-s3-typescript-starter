e57860632579d74e21c3578a73941c11a59f5ff2
MiguelHigueraDev <133175356+MiguelHigueraDev@users.noreply.github.com>
2025-07-18 16:39:06 -0400

    feat: implement handlerUploadThumbnail

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -1,9 +1,9 @@
 import { getBearerToken, validateJWT } from "../auth";
 import { respondWithJSON } from "./json";
-import { getVideo } from "../db/videos";
+import { getVideo, updateVideo } from "../db/videos";
 import type { ApiConfig } from "../config";
 import type { BunRequest } from "bun";
-import { BadRequestError, NotFoundError } from "./errors";
+import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
 
 type Thumbnail = {
   data: ArrayBuffer;
@@ -47,7 +47,42 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
 
   console.log("uploading thumbnail for video", videoId, "by user", userID);
 
-  // TODO: implement the upload here
+  const formData = await req.formData();
+  const file = formData.get("thumbnail");
 
-  return respondWithJSON(200, null);
+  if (!file || !(file instanceof File)) {
+    throw new BadRequestError("No file provided");
+  }
+
+  // 10MB
+  const maxFileSize = 10 << 20;
+
+  if (file.size > maxFileSize) {
+    throw new BadRequestError("File too large");
+  }
+
+  const buffer = await file.arrayBuffer();
+  const videoMetadata = getVideo(cfg.db, videoId);
+
+  if (!videoMetadata || userID !== videoMetadata.userID) {
+    throw new UserForbiddenError("User does not own video");
+  }
+
+  const thumbnail = {
+    data: buffer,
+    mediaType: file.type,
+  };
+
+  videoThumbnails.set(videoId, thumbnail);
+
+  const thumbnailURL = `http://localhost:8091/api/thumbnails/${videoId}`;
+
+  updateVideo(cfg.db, {
+    ...videoMetadata,
+    thumbnailURL,
+  });
+
+  const updatedVideo = getVideo(cfg.db, videoId);
+
+  return respondWithJSON(200, updatedVideo);
 }
